95b90dca326b380b9b71c391085eca84f0a6c33a
niktos12 <nikitosss121207@gmail.com>
2023-12-19 20:19:25 +0400

    ///

--- my-app/src/components/WeeklyTop.tsx
+++ my-app/src/components/WeeklyTop.tsx
@@ -5,12 +5,8 @@ import SwiperCore from 'swiper';
 import { useEffect, useState } from 'react';
 
 export function WeeklyTop(){
-    const [theme,setTheme] = useState<'light'| 'dark'>('light');
-    useEffect(()=> {
-        document.body.className = theme;
-    }, [theme])
     return(
-        <div className={`${theme === 'dark' ? 'bg-[#12141D]' : 'bg-[#F1F1F1]'} flex flex-col justify-center items-center swiper-bg-change`}>
+        <div className={` flex flex-col justify-center items-center swiper-bg-change`}>
             <h1 className="text-4xl font-semibold text-[#C5C5C5] mt-16">Weekly - Top NFT</h1>
             <SwiperWeeklyTop/>
         </div>
